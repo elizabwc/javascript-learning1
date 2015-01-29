47fb245369f6f9fcee4e45c3967bbfec9de9857c
Williams <elizabwc@jomc7912.dhcp.unc.edu>
2015-01-29 08:37:01 -0500

    adding files from class

---
+++ script.js
@@ -0,0 +1,24 @@
+//this file is used for teaching basic commands.
+
+var userName = " Elizabeth";
+
+var todayDate = new Date();
+
+
+function myFunction(){
+   document.write("Hello");
+   document.write(userName);
+   console.log(todayDate);
+}
+
+
+document.getElementById("demo").innerHTML="this is the new info"; 
+document.getElementById("demo2").innerHTML="hello elizabeth";
+myFunction();
+
+var myNumberVariable = 5;
+var myTextVariable = "hello";
+var myDateVariable = new Date();
+
+console.log(myNumberVariable);
+console.log(myTextVariable);
---
+++ script2.js
@@ -0,0 +1,13 @@
+var userName = "Elizabeth";
+var todayDate = new Date();
+
+function myFunction(){
+   document.write(userName);
+   document.write(todayDate);
+}
+
+document.getElementById("hello").innerHTML="Hello, Elizabeth";
+document.getElementById("date").innerHTML= todayDate;
+var myTextVariable = "Hello, Your Name";
+
+
